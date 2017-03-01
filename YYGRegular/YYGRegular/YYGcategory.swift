85ed03db5ad2aa0cd6c161327df480656a206017
yinyuguang <291181204@qq.com>
2017-03-01 15:27:28 +0800

    11

--- YYGRegular/YYGRegular/YYGcategory.swift
+++ YYGRegular/YYGRegular/YYGcategory.swift
@@ -100,4 +100,36 @@ extension UIView {
     }
 }
 
+// MARK: - Convenience methods for UIColor
+public extension UIColor {
+    
+    /// Init color without divide 255.0
+    ///
+    /// - Parameters:
+    ///   - r: (0 ~ 255) red
+    ///   - g: (0 ~ 255) green
+    ///   - b: (0 ~ 255) blue
+    ///   - a: (0 ~ 1) alpha
+    convenience init(r: Int, g: Int, b: Int, a: CGFloat) {
+        self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: a)
+    }
+    
+    /// Init color without divide 255.0
+    ///
+    /// - Parameters:
+    ///   - r: (0 ~ 255) red
+    ///   - g: (0 ~ 255) green
+    ///   - b: (0 ~ 1) alpha
+    convenience init(r: Int, g: Int, b: Int) {
+        self.init(r: r, g: g, b: b, a: 1)
+    }
+    
+    /// Init color with hex code
+    ///
+    /// - Parameter hex: hex code (eg. 0x00eeee)
+    convenience init(hex: Int) {
+        self.init(r: (hex & 0xff0000) >> 16, g: (hex & 0xff00) >> 8, b: (hex & 0xff), a: 1)
+    }
+}
+
 		
